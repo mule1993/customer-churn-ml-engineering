4557ca75d576f3df9b03b9a0dc992a65ffbd29cb
mule1993 <71659980+mule1993@users.noreply.github.com>
2025-12-23 10:24:39 +0300

    Add model evaluation module

--- src/models/evaluate.py
+++ src/models/evaluate.py
@@ -1,1 +1,17 @@
+from sklearn.metrics import roc_auc_score, classification_report
 
+
+def evaluate_model(model, X_test, y_test):
+    """
+    Evaluate trained model.
+    """
+
+    y_pred = model.predict(X_test)
+
+    roc_auc = roc_auc_score(y_test, y_pred)
+    report = classification_report(y_test, y_pred)
+
+    return {
+        "roc_auc": roc_auc,
+        "report": report,
+    }
