73caf7b1c07fbc29974f9bd1204c8c78d923be49
mule1993 <muluneh.gashaw@gmail.com>
2025-12-25 08:00:49 +0000

    Update

---
+++ tests/test_schema.py
@@ -0,0 +1,13 @@
+# tests/test_schema.py
+
+import pandas as pd
+import pytest
+
+from src.utils.schema import validate_schema
+
+
+def test_schema_missing_column():
+    df = pd.DataFrame({"tenure": [1]})
+
+    with pytest.raises(ValueError):
+        validate_schema(df)
